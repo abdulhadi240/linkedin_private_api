bba9addf4b67d8d8741bf5753717d35a8ae1f71c
Ejaz Ullah <46069373+Ejazullah42@users.noreply.github.com>
2025-06-09 19:12:47 +0500

    Update main.py
    Update Chanage And Update Proxy and Headers and add User Agent

--- main.py
+++ main.py
@@ -17,18 +17,29 @@ logger = logging.getLogger(__name__)
 batch_jobs = {}
 
 
-def process_profiles(batch_id, jsessionid, li_at, profile_urls, proxy=None):
+# def proxy_format(proxy_string):
+#     try:
+#         host, port, username, password = proxy_string.split(":")
+#         return f"http://{username}:{password}@{host}:{port}"
+#     except ValueError:
+#         raise ValueError("Invalid proxy string format. Expected format: host:port:username:password")
+def process_profiles(batch_id,user_agent, jsessionid, li_at, profile_urls, proxy=None):
     try:
         # Set up headers for LinkedIn API
+
         headers = {
             'accept': 'application/vnd.linkedin.normalized+json+2.1',
             'accept-language': 'en-US,en;q=0.9',
             'csrf-token': f'ajax:{jsessionid}',
-            'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/135.0.0.0 Safari/537.36',
-            'cookie': f'JSESSIONID="{jsessionid}";li_at={li_at}',
+            'priority': 'u=1, i',
+            'user-agent': user_agent,
+            # 'x-li-track': '{"clientVersion":"1.13.36147","mpVersion":"1.13.36147","osName":"web","timezoneOffset":-5,"timezone":"America/Chicago","deviceFormFactor":"DESKTOP","mpName":"voyager-web","displayDensity":2,"displayWidth":2940,"displayHeight":1912}',
+            'x-restli-protocol-version': '2.0.0',
+            # 'cookie': 'bcookie="v=2&73224489-5d3a-4ef1-8484-1fa0197e4b00"; bscookie="v=1&20250214121209b47d986c-1bee-4064-821d-0874f2698a7dAQFL-Tf0UsTTgTqNBBQoU8igWfWtkHT7"; li_rm=AQFa3Nun3KiwHgAAAZUEZDP9fw3BG1qyGeet_1JJ2QHBRMmHrOHODz-t_uoCjQO0O4BNPA4jgM4zs9bjo9GjsYF-5VU-Bzp5YtSsywwgrLcc-r1ihdeF0mVQkMnW1uIK8rf5Ty-sg3R8X39HO2jqnuIa3CQkKvCLUlDgVK2QkYfrcbsmIt_uhF40O6vHM6-zwhu5NNgbuDWLvtlUPTaWM-bxck7QjrdZ381AjIunAuJF_UlFTeN7wiBDmVBjzHTouYtQJiSlB0gU4GLYQ8MhKIaTXAGk1oK--oLMEULJEJh0eU4XjSVH4cNtElPQiuGhviEu_G3zeZ52RZCPrjg; JSESSIONID="ajax:5407826422185488693"; li_theme=light; li_theme_set=app; li_sugr=76a61826-9c4f-4ad1-b4b9-4039569c8c10; dfpfpt=79fa1693f80f42499405ca8de1406c1e; lang=v=2&lang=en-us; li_at=AQEDATEFsqEF7K_tAAABl1TfUbYAAAGXeOvVtk0ARU5xZLOVZkGfAuRAj4ANklkZFHjjDHa0JoRciKY9r9ZSbcN80RMdKsqb0KMAKYxo58HbJlHYVh_4gMh_8XwaZrQbt8gYEH-4Ayf-RGJB9GR-94Z2; liap=true; timezone=America/Chicago; UserMatchHistory=AQLRxwUYIf5F6AAAAZdU34VV-dXse2FMU3XyceEqRKctW-rCf2tBeHJDFZjNrW01nynw1g9RSlKcfaozo1nXak98p-t8yClxMdMeiZ5bwZL9gL9o64iBAwKE__EmIUErdZbHCx1qOVl5E78_CcUGiTii0OdlLXXEIJmp86ap6YlR6kZJ0StQHgBM_tqO_ni_b4EscmngInykRtPzaj6RaW7pPKiowg6XR4z0sAmWwDmYHDEe1H66gkUMBu6981FnH2yLriWdIEWR7aF5-B2fQbp0re0qlY3G4fz01DRzkyLj8UYLpJVjAOdICBm9tTqlLbmM8F2acgAa51D10_xUTqnlfHHf4PWMf-IB_d0vi4L09gqIww; AnalyticsSyncHistory=AQI2uSWRp7MpCAAAAZdU34VVKlZR2zzXh0NbG_llQ4RkjqqDfEVlMGpzyC3bxb-18SmP2PjmSOjEoXgADQvg4w; lms_ads=AQGS-EavGybxZgAAAZdU34ZFfjnBHOcg7K_UOjmszgJNloBGPB4ehfkzRi2yg7wtLKoYYjo5r518ranzttbp66UsV84zc0fC; lms_analytics=AQGS-EavGybxZgAAAZdU34ZFfjnBHOcg7K_UOjmszgJNloBGPB4ehfkzRi2yg7wtLKoYYjo5r518ranzttbp66UsV84zc0fC; _guid=e91f06f3-6ae0-4870-921b-f5e265e593bd; lidc="b=VB93:s=V:r=V:a=V:p=V:g=4191:u=571:x=1:i=1749475630:t=1749499751:v=2:sig=AQGDLz1rnrDNUcwXMWDyRrRP-VTVUunm"; AMCVS_14215E3D5995C57C0A495C55%40AdobeOrg=1; AMCV_14215E3D5995C57C0A495C55%40AdobeOrg=-637568504%7CMCIDTS%7C20249%7CMCMID%7C23216564369143702163963164319688294988%7CMCAAMLH-1750080431%7C7%7CMCAAMB-1750080431%7C6G1ynYcLPuiQxYZrsz_pkqfLG9yMXBpb2zX5dvJdYQJzPXImdj0y%7CMCOPTOUT-1749482831s%7CNONE%7CvVersion%7C5.1.1%7CMCCIDH%7C1476003076; aam_uuid=23753684545514550753904853766321795463; _gcl_au=1.1.517968955.1749475634; fptctx2=taBcrIH61PuCVH7eNCyH0Iitb%252bEMfwlgK%252fM8w%252f28EbfzJKkBisHlSHSWXKdZciWrcg6m5N55K%252bEhloH7u%252fjFFvpRsDb2RA9UxCnSpqup1ldhL1Ax%252fMvrSNDkDW3rDLkR73fsYdBZ%252b91eanptkzPO4vElAzHyfgx%252fAQu919glw9ojnPp7JVgSJJ0rh1F7Hav5dakKQJ5NM4St5rZyrh8wmQY3pqi8RlvFpRRQZoJDPaTDjm5ZBchdpr74K%252biDqwsKpOwNXAnj%252ffIZ7MnJbdHN4i1gP404OK6s9fjQYJsUlFU2oMmwL3dwYsh6%252fhbjIZKIWM6oI9y1w3g2o9UE9BUmat6JlxIGaDrSbjqL1K5QkwE%253d; __cf_bm=t_HIZk4REVNjtgixRmW8ebQh2ZH3AFYKa2gouVoX3qQ-1749476509-1.0.1.1-zQUAghOhT6f87r4EiTiSwXbUzMcAKK.zYD2BwOWBnl8LIVL5VKGp5QVWPO5CYzKZ0LkmdEYB5qYJ_AX.uI1XFgcwFe12yH3STyzPTyf6s_4',
+            'cookie': f'JSESSIONID="ajax:{jsessionid}";li_at={li_at}',
         }
-
         # Initialize LinkedIn API client with proxy string
+
         api = Linkedin(headers=headers, proxy=proxy)
         all_profiles = []
 
@@ -75,6 +86,7 @@ def scrape_linkedin():
         # Extract JSESSIONID, li_at, profile_urls, and proxy
         jsessionid = data.get('JSESSIONID')
         li_at = data.get('li_at')
+        user_agent=data.get('user_agent')
         profile_urls = data.get('profile_urls')
         proxy = data.get('proxy')  # Proxy string in format host:port:username:password
 
@@ -100,7 +112,7 @@ def scrape_linkedin():
         # Start background processing
         threading.Thread(
             target=process_profiles,
-            args=(batch_id, jsessionid, li_at, profile_urls, proxy),
+            args=(batch_id,user_agent,jsessionid, li_at, profile_urls, proxy),
             daemon=True
         ).start()
 
@@ -149,5 +161,6 @@ def check_status(batch_id):
 
 
 if __name__ == '__main__':
+
     app.run(debug=True, host='0.0.0.0', port=5000)
 
